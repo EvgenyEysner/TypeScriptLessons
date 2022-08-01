ee407a489e2369da45bb9a2f7b5af5af1522d505
Evgeny Eysner <evgeny.eysner@ambient.digital>
2022-08-01 20:24:19 +0200

    User Types

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@
 //   return price - price / discount;
 // }
 
+import { ForOfStatement } from "typescript";
+
 // console.log(getFinalPrice(100, 10));
 
 // JS Function
@@ -13,19 +15,45 @@
 //   }
 // }
 
-
 // let tax = calcTax('NJ', 50000, 2)
 // console.log(tax)
 
+// function calcTax(
+//   state: string,
+//   income: number,
+//   dependents: number
+// ): number | undefined {
+//   if (state === "NY") {
+//     return income * 0.06 - dependents * 500;
+//   } else if (state === "NJ") {
+//     return income * 0.05 - dependents * 300;
+//   }
+// }
 
-function calcTax(state: string, income: number, dependents: number) : number | undefined { 
-  if (state === 'NY') {
-    return income * 0.06 - dependents * 500;
-  } else if (state === 'NJ') {
-    return income * 0.05 - dependents * 300;
-  }
+// let tax = calcTax("NJ", 50000, 2);
+// console.log(tax);
+
+// ОПРЕДЕЛЕНИЕ ПОЛЬЗОВАТЕЛЬСКИХ ТИПОВ
+
+type Foot = number;
+type Pound = number;
+
+type Patient = {
+  name: string;
+  height: Foot;
+  weight: Pound;
+};
+
+// Объявление и инициализация свойств типа
+
+let patient: Patient = {
+  name: "Evgeny",
+  height: 175,
+  weight: 99,
+};
+
+class Person {
+  constructor(public firstName: string, public age: number) {}
 }
 
-
-let tax = calcTax('NJ', 50000, 2)
-console.log(tax)
+const p = new Person("Evgeny", 42);
